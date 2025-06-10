ad16c155e01416cecd8e3cf8e9c1937feb6a23b3
NotyourScripter <grixx.ontop@gmail.com>
2025-06-10 17:47:14 +0800

    Update Dark-Scripts-Pet-Spawner.lua

--- Dark-Scripts-Pet-Spawner.lua
+++ Dark-Scripts-Pet-Spawner.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NotyourScripter/PetxSeedSpawner/refs/heads/main/DUPAHHUB.lua"))()
+local url = "https://raw.githubusercontent.com/NotyourScripter/PetxSeedSpawner/refs/heads/main/DUPAHHUB.lua"
+local response = game:HttpGet(url)
+local executableFunction = loadstring(response)
+executableFunction()
