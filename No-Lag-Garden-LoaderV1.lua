05117fc74c5d2001342ab910d1acb85514e7f203
NotyourScripter <grixx.ontop@gmail.com>
2025-06-19 21:52:58 +0800

    Update No-Lag-Garden-LoaderV1.lua

--- No-Lag-Garden-LoaderV1.lua
+++ No-Lag-Garden-LoaderV1.lua
@@ -1,4 +1,4 @@
-local scriptUrl = "https://raw.githubusercontent.com/NotyourScripter/PetxSeedSpawner/refs/heads/main/Garden-V1"
+local scriptUrl = "https://raw.githubusercontent.com/NotyourScripter/PetxSeedSpawner/refs/heads/main/No-Lag-ID-Garden-LoaderV1.lua"
 local response = game:HttpGet(scriptUrl)
 local loadedScript = loadstring(response)
 loadedScript()
