c3d6f6303add59ae40f4b8cb4d4347db2349fb85
NotyourScripter <grixx.ontop@gmail.com>
2025-07-20 00:26:22 +0800

    Delete No-Lag-Garden-LoaderV1.lua

--- No-Lag-Garden-LoaderV1.lua
+++
@@ -1,4 +0,0 @@
-local scriptUrl = "https://raw.githubusercontent.com/NotyourScripter/PetxSeedSpawner/refs/heads/main/No-Lag-ID-Garden-LoaderV1.lua"
-local response = game:HttpGet(scriptUrl)
-local loadedScript = loadstring(response)
-loadedScript()
